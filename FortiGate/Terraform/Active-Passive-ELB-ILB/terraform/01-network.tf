6badf955354257d8e843251ce4302f8665f831a0
Theodor Wulff <theodor.wulff@qbeyond.de>
2022-05-04 14:41:12 +0200

    using our naming convention

--- FortiGate/Terraform/Active-Passive-ELB-ILB/terraform/01-network.tf
+++ FortiGate/Terraform/Active-Passive-ELB-ILB/terraform/01-network.tf
@@ -10,49 +10,49 @@
 ##############################################################################################################
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${var.PREFIX}-VNET"
+  name                = "vnet-${replace(replace(var.vnet, ".", "-"), "/", "-")}-${azurerm_resource_group.resourcegroup.location}"
   address_space       = [var.vnet]
   location            = azurerm_resource_group.resourcegroup.location
   resource_group_name = azurerm_resource_group.resourcegroup.name
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "${var.PREFIX}-SUBNET-FGT-EXTERNAL"
+  name                 = "snet-${replace(replace(var.subnet["1"], ".", "-"), "/", "-")}-fgt-External"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet["1"]]
 }
 
 resource "azurerm_subnet" "subnet2" {
-  name                 = "${var.PREFIX}-SUBNET-FGT-INTERNAL"
+  name                 = "snet-${replace(replace(var.subnet["2"], ".", "-"), "/", "-")}-fgt-Internal"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet["2"]]
 }
 
 resource "azurerm_subnet" "subnet3" {
-  name                 = "${var.PREFIX}-SUBNET-FGT-HASYNC"
+  name                 = "snet-${replace(replace(var.subnet["3"], ".", "-"), "/", "-")}-fgt-HASync"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet["3"]]
 }
 
 resource "azurerm_subnet" "subnet4" {
-  name                 = "${var.PREFIX}-SUBNET-FGT-MGMT"
+  name                 = "snet-${replace(replace(var.subnet["4"], ".", "-"), "/", "-")}-fgt-Management"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet["4"]]
 }
 
 resource "azurerm_subnet" "subnet5" {
-  name                 = "${var.PREFIX}-SUBNET-PROTECTED-A"
+  name                 = "snet-${replace(replace(var.subnet["5"], ".", "-"), "/", "-")}-fgt-Protected-A"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet["5"]]
 }
 
 resource "azurerm_subnet" "subnet6" {
-  name                 = "${var.PREFIX}-SUBNET-PROTECTED-B"
+  name                 = "snet-${replace(replace(var.subnet["6"], ".", "-"), "/", "-")}-fgt-Protected-B"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet["6"]]
@@ -64,7 +64,7 @@ resource "azurerm_subnet_route_table_association" "subnet5rt" {
 }
 
 resource "azurerm_route_table" "protectedaroute" {
-  name                = "${var.PREFIX}-RT-PROTECTED-A"
+  name                = "rt-${azurerm_subnet.subnet5.name}"
   location            = var.LOCATION
   resource_group_name = azurerm_resource_group.resourcegroup.name
 
@@ -93,7 +93,7 @@ resource "azurerm_subnet_route_table_association" "subnet6rt" {
 }
 
 resource "azurerm_route_table" "protectedbroute" {
-  name                = "${var.PREFIX}-RT-PROTECTED-B"
+  name                = "rt-${azurerm_subnet.subnet6.name}"
   location            = var.LOCATION
   resource_group_name = azurerm_resource_group.resourcegroup.name
 
